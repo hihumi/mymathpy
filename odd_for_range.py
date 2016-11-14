0331b405cd9c3cafde6b506c3802174a6e1d2a0b
hihumi <sth@sth-no-MacBook-ProProPro.local>
2016-11-15 05:02:32 +0900

    add file

---
+++ odd_for_range.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def odd_for_range(x):
+    """奇数をforとrangeで出力する関数。
+
+    奇数: 2で割り切れない整数を奇数。2で割ると余りが1。
+    """
+
+    for i in range(x):
+        if i % 2 == 1:
+            print(i)
+
+if __name__ == '__main__':
+    odd_for_range(10)
