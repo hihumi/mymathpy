90323efba862a969ba8123863b295ab9d6eece2e
hihumi <sth@sth-no-MacBook-ProProPro.local>
2016-11-15 05:09:14 +0900

    update

--- tests/test_myfunctions.py
+++ tests/test_myfunctions.py
@@ -5,9 +5,27 @@
 """
 
 import unittest
+
 import even_for_range
+import odd_for_range
 
 class TestMyFunctions(unittest.TestCase):
+
+    # test_funcs_name
+    """
+    def test_new_func(self):
+        self.assert___
+
+
+        NOTE: Indent
+        def t_n_f():
+           do something
+
+        expected = ___
+        actual   = ___
+        self.assert___
+
+    """
 
     # even_for_range
     def test_even_for_range(self):
@@ -23,21 +41,19 @@ class TestMyFunctions(unittest.TestCase):
         actual   = even_for_range.even_for_range(10)
         self.assertEqual(expected,  actual)
 
-    # other
-    """
-    def test_new_func(self):
-        self.assert___
+    def test_odd_for_range(self):
+        self.assertEqual(odd_for_range.odd_for_range(10), odd_for_range.odd_for_range(10))
+        print()
 
+        def t_o_f_r(x):
+            for i in range(x):
+                if i % 2 == 1:
+                    print(i)
 
-        NOTE: Indent
-        def t_n_f():
-           do something
+        expected = t_o_f_r(10)
+        actual   = odd_for_range.odd_for_range(10)
+        self.assertEqual(expected, actual)
 
-        expected = ___
-        actual   = ___
-        self.assert___
-
-    """
 
 
 
