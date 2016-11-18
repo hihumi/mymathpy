9fee338afd531cf06932abce0c6e12f03e1ab882
hihumi <sth@sth-no-MacBook-ProProPro.local>
2016-11-18 23:26:05 +0900

    update

--- tests/test_myallfunctions.py
+++ tests/test_myallfunctions.py
@@ -13,6 +13,7 @@ import even_for_range
 import odd_for_range
 import mod_three
 import even_or_odd
+import classify_mod_four
 
 class TestMyAllFunctions(unittest.TestCase):
 
@@ -90,6 +91,7 @@ class TestMyAllFunctions(unittest.TestCase):
     # even_or_odd
     def test_even_or_odd(self):
         self.assertEqual(even_or_odd.even_or_odd(10), even_or_odd.even_or_odd(10))
+        print()
 
         def t_e_o_o(x):
             counter = 0
@@ -102,6 +104,39 @@ class TestMyAllFunctions(unittest.TestCase):
 
         expected = t_e_o_o(10)
         actual   = even_or_odd.even_or_odd(10)
+        self.assertEqual(expected, actual)
+
+    def test_classify_mod_for(self):
+        self.assertEqual(classify_mod_four.classify_mod_four(15), classify_mod_four.classify_mod_four(15))
+        print()
+
+        def c_m_f(x):
+            print('num: 0から{0}'.format(x))
+
+            # 余り(remainder)を収めるリスト。
+            r_0 = []
+            r_1 = []
+            r_2 = []
+            r_3 = []
+
+            counter = 0
+            while counter <= x:
+                if counter % 4 == 0:
+                    r_0.append(counter)
+                elif counter % 4 == 1:
+                    r_1.append(counter)
+                elif counter % 4 == 2:
+                    r_2.append(counter)
+                elif counter % 4 == 3:
+                    r_3.append(counter)
+                counter += 1
+            print("余り0: {0}".format(r_0))
+            print('余り1: {0}'.format(r_1))
+            print('余り2: {0}'.format(r_2))
+            print('余り3: {0}'.format(r_3))
+
+        expected = c_m_f(15)
+        actual   = classify_mod_four.classify_mod_four(15)
         self.assertEqual(expected, actual)
 
 """
