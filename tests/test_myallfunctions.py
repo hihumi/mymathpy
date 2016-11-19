9ec9ccf4bcac7ff5f9245341acac0efd5bca1be1
hihumi <sth@sth-no-MacBook-ProProPro.local>
2016-11-20 04:20:54 +0900

    update

--- tests/test_myallfunctions.py
+++ tests/test_myallfunctions.py
@@ -16,6 +16,7 @@ import mod_three
 import even_or_odd
 import classify_mod_four
 import multi_of_seven
+import multi_of_6_from_1_to_100
 
 class TestMyAllFunctions(unittest.TestCase):
 
@@ -162,6 +163,20 @@ class TestMyAllFunctions(unittest.TestCase):
         actual   = multi_of_seven.multi_of_seven(seven = 7, begin = 0, end = 9)
         self.assertEqual(expected, actual)
 
+
+    # multi_of_6_from_1_to_100
+    def test_multi_of_6_from_1_to_100(self):
+        self.assertEqual(multi_of_6_from_1_to_100.multi_of_6_from_1_to_100(), multi_of_6_from_1_to_100.multi_of_6_from_1_to_100())
+
+        def m_o_6_f_1_t_100(x = 100):
+            res1 = [i for i in range(1, x + 1) if i % 6 == 0]
+            print('1から100までの6の倍数: {0}'.format(res1))
+            length = len(res1)
+            print('個数: {0}'.format(length))
+
+        expected = m_o_6_f_1_t_100()
+        actual   = multi_of_6_from_1_to_100.multi_of_6_from_1_to_100()
+        self.assertEqual(expected, actual)
 
 """
 if __name__ == '__main__':
