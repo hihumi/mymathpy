6167a8b33bca92c2564ed649f69025533b14d392
hihumi <sth@sth-no-MacBook-ProProPro.local>
2016-11-24 04:23:10 +0900

    update

--- tests/test_myallfunctions.py
+++ tests/test_myallfunctions.py
@@ -18,6 +18,7 @@ import classify_mod_four
 import multi_of_seven
 import multi_of_6_from_1_to_100
 import common_multiple_4_8
+import common_multiple_8_12
 
 class TestMyAllFunctions(unittest.TestCase):
 
@@ -201,6 +202,24 @@ class TestMyAllFunctions(unittest.TestCase):
 
         expected = c_m_4_8(25, 50)
         actual   = common_multiple_4_8.common_multiple_4_8(25, 50)
+        self.assertEqual(expected, actual)
+
+    def test_common_multiple_8_12(self):
+        self.assertEqual(common_multiple_8_12.common_multiple_8_12(50, 60), common_multiple_8_12.common_multiple_8_12(50, 60))
+        print()
+
+        def c_m_8_12(eight, twelve):
+            common_multiple_8 = [i for i in range(1, eight + 1) if i % 8 == 0]
+            common_multiple_12 = [j for j in range(1, twelve + 1) if j % 12 == 0]
+
+            c_m_8_set = set(common_multiple_8)
+            c_m_12_set = set(common_multiple_12)
+            c_m_8_12_intersec = c_m_8_set.intersection(c_m_12_set)
+            res = min(c_m_8_12_intersec)
+            print('8と12の最小公倍数: {0}'.format(res))
+
+        expected = c_m_8_12(50, 60)
+        actual   = common_multiple_8_12.common_multiple_8_12(50, 60)
         self.assertEqual(expected, actual)
 
 
