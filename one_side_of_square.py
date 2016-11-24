f94ff651a15e7d81bcd56e55d0889f9c0e7e2262
hihumi <sth@sth-no-MacBook-ProProPro.local>
2016-11-24 11:18:51 +0900

    add file

---
+++ one_side_of_square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""算数問題: 縦6cm, 横9cmの長方形のブロックを隙間なく敷き詰めて、
+可能な限り小さい正方形を作りなさい。このとき、正方形の1辺の長さは何cmになるか。
+"""
+
+def one_side_of_square():
+    """正方形の1辺の長さを求める関数
+
+    引数:
+        なし
+
+    返り値, 戻り値:
+        なし
+
+    例:
+        one_side_of_square()
+        実行結果: 正方形1辺の長さ: 18
+
+    単体テスト: doctest:
+    >>> one_side_of_square()
+    正方形1辺の長さ: 18
+    """
+
+    height = 6
+    width  = 9
+
+    common_multiple_6 = [i for i in range(1, 51) if i % height == 0]
+    common_multiple_9 = [j for j in range(1, 51) if j % width == 0]
+
+    # 以下、主に変数名は頭文字を使用
+    c_m_6_set = set(common_multiple_6)
+    c_m_9_set = set(common_multiple_9)
+    res = min(c_m_6_set.intersection(c_m_9_set))
+    print('正方形1辺の長さ: {0}'.format(res))
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    one_side_of_square()
--- tests/test_common_multiple_8_12.py
+++ tests/test_common_multiple_8_12.py
@@ -2,14 +2,14 @@
 # -*- coding: utf-8 -*-
 
 import unittest
-import common_multiple_8_12
+import lowest_common_multiple_8_12
 
-class TestCommonMultiple_8_12(unittest.TestCase):
-    def test_common_multiple_8_12(self):
-        self.assertEqual(common_multiple_8_12.common_multiple_8_12(50, 60), common_multiple_8_12.common_multiple_8_12(50, 60))
+class TestLowestCommonMultiple_8_12(unittest.TestCase):
+    def test_lowest_common_multiple_8_12(self):
+        self.assertEqual(lowest_common_multiple_8_12.lowest_common_multiple_8_12(50, 60), lowest_common_multiple_8_12.lowest_common_multiple_8_12(50, 60))
         print()
 
-        def c_m_8_12(eight, twelve):
+        def l_c_m_8_12(eight, twelve):
             common_multiple_8 = [i for i in range(1, eight + 1) if i % 8 == 0]
             common_multiple_12 = [j for j in range(1, twelve + 1) if j % 12 == 0]
 
@@ -19,6 +19,6 @@ class TestCommonMultiple_8_12(unittest.TestCase):
             res = min(c_m_8_12_intersec)
             print('8と12の最小公倍数: {0}'.format(res))
 
-        expected = c_m_8_12(50, 60)
-        actual   = common_multiple_8_12.common_multiple_8_12(50, 60)
+        expected = l_c_m_8_12(50, 60)
+        actual   = lowest_common_multiple_8_12.lowest_common_multiple_8_12(50, 60)
         self.assertEqual(expected, actual)
